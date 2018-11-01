a46ff1f0502d38a3553ee12d59340545faa81482
Yuan Fu <casouri@gmail.com>
2018-11-01 12:11:57 -0400

    Add dash

--- uikit.el
+++ uikit.el
@@ -1,27 +1,30 @@
 ;;; uikit.el --- UI Kit for Emacs      -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2018  Yuan Fu
-
+;;
 ;; Author: Yuan Fu <casouri@gmail.com>
-
+;;
+;; Package-Requires: ((dash "2.14.1"))
+;;
 ;;; This file is NOT part of GNU Emacs
-
+;;
 ;;; License
 ;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
 ;; Floor, Boston, MA 02110-1301, USA.
+
 
 ;;; Commentary:
 ;;
@@ -31,6 +34,7 @@
 
 
 (require 'eieio-base)
+(require 'dash)
 
 ;;; Customize
 
